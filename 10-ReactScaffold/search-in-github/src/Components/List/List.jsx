a2bfded807fcdced5965e515428b2a7f427f45c8
cxdeng <chengx.deng@icloud.com>
2023-08-23 05:39:07 +0100

    Components and Style

--- 10-ReactScaffold/search-in-github/src/Components/List/List.jsx
+++ 10-ReactScaffold/search-in-github/src/Components/List/List.jsx
@@ -1,10 +1,88 @@
 import React, { Component } from 'react'
+import list from './List.module.css'
+import picture from '../../img/Leslie.webp'
 
 export default class List extends Component {
   render() {
     return (
-      <div>
-        
+      <div className={list['list-container']}>
+        <div className={list.card}>
+          <a href="">
+            <img src={picture} alt="avatar" />
+          </a>
+          <div>Leslie</div>
+        </div>
+
+        <div className={list.card}>
+          <a href="">
+            <img src={picture} alt="" />
+          </a>
+          <div>Leslie</div>
+        </div>
+
+        <div className={list.card}>
+          <a href="">
+            <img src={picture} alt="" />
+          </a>
+          <div>Leslie</div>
+        </div>
+
+        <div className={list.card}>
+          <a href="">
+            <img src={picture} alt="" />
+          </a>
+          <div>Leslie</div>
+        </div>
+
+        <div className={list.card}>
+          <a href="">
+            <img src={picture} alt="" />
+          </a>
+          <div>Leslie</div>
+        </div>
+
+        <div className={list.card}>
+          <a href="">
+            <img src={picture} alt="" />
+          </a>
+          <div>Leslie</div>
+        </div>
+
+        <div className={list.card}>
+          <a href="">
+            <img src={picture} alt="" />
+          </a>
+          <div>Leslie</div>
+        </div>
+
+        <div className={list.card}>
+          <a href="">
+            <img src={picture} alt="" />
+          </a>
+          <div>Leslie</div>
+        </div>
+
+        <div className={list.card}>
+          <a href="">
+            <img src={picture} alt="" />
+          </a>
+          <div>Leslie</div>
+        </div>
+
+        <div className={list.card}>
+          <a href="">
+            <img src={picture} alt="" />
+          </a>
+          <div>Leslie</div>
+        </div>
+
+        <div className={list.card}>
+          <a href="">
+            <img src={picture} alt="" />
+          </a>
+          <div>Leslie</div>
+        </div>
+
       </div>
     )
   }
--- 10-ReactScaffold/search-in-github/src/Components/Search/Search.jsx
+++ 10-ReactScaffold/search-in-github/src/Components/Search/Search.jsx
@@ -1,11 +1,33 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import search from './Search.module.css'
 
 export default class Search extends Component {
   render() {
     return (
-      <div>
-        
+      <div className={search['search-container']}>
+        <h1>Search Users in Github</h1>
+        <div>
+          <input ref={cur => this.keywordElem = cur} type="text" placeholder='Enter User Name' />
+          <button onClick={this.search}>Search</button>
+        </div>
       </div>
     )
+  }
+
+  search = () => {
+    // 获取用户的输入
+    const { value: keyword } = this.keywordElem
+
+    // 发送网络请求
+    axios.get(`https://api.github.com/search/users?q=${keyword}`)
+      .then(
+        response => {
+          console.log(response.data);
+        },
+        error => {
+          console.log(error);
+        }
+      )
   }
 }
