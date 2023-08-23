3e791c95466a2b5262364c7c400dd1bf62276a6c
cxdeng <chengx.deng@icloud.com>
2023-08-23 07:24:26 +0100

    The first Edition of 'Search Users in Github' Project

--- 10-ReactScaffold/search-in-github/src/Components/List/List.jsx
+++ 10-ReactScaffold/search-in-github/src/Components/List/List.jsx
@@ -4,86 +4,37 @@ import picture from '../../img/Leslie.webp'
 
 export default class List extends Component {
   render() {
-    return (
-      <div className={list['list-container']}>
-        <div className={list.card}>
-          <a href="">
-            <img src={picture} alt="avatar" />
-          </a>
-          <div>Leslie</div>
+    const { users, isFirst, isLoading, err } = this.props
+    if (isFirst) {
+      return (
+        <h2>Input username and search</h2>
+      )
+    } else if (isLoading) {
+      return (
+        <h2>Loading</h2>
+      )
+    } else if (err) {
+      return (
+        <h2>{err}</h2>
+      )
+    } else {
+      return (
+        <div className={list['list-container']}>
+          {
+            users.map((userItem) => {
+              return (
+                <div key={userItem.id} className={list.card}>
+                  <a href={userItem.html_url}>
+                    <img src={userItem.avatar_url} alt="avatar" />
+                  </a>
+                  <div>{userItem.login}</div>
+                </div>
+              )
+            })
+          }
         </div>
+      )
+    }
 
-        <div className={list.card}>
-          <a href="">
-            <img src={picture} alt="" />
-          </a>
-          <div>Leslie</div>
-        </div>
-
-        <div className={list.card}>
-          <a href="">
-            <img src={picture} alt="" />
-          </a>
-          <div>Leslie</div>
-        </div>
-
-        <div className={list.card}>
-          <a href="">
-            <img src={picture} alt="" />
-          </a>
-          <div>Leslie</div>
-        </div>
-
-        <div className={list.card}>
-          <a href="">
-            <img src={picture} alt="" />
-          </a>
-          <div>Leslie</div>
-        </div>
-
-        <div className={list.card}>
-          <a href="">
-            <img src={picture} alt="" />
-          </a>
-          <div>Leslie</div>
-        </div>
-
-        <div className={list.card}>
-          <a href="">
-            <img src={picture} alt="" />
-          </a>
-          <div>Leslie</div>
-        </div>
-
-        <div className={list.card}>
-          <a href="">
-            <img src={picture} alt="" />
-          </a>
-          <div>Leslie</div>
-        </div>
-
-        <div className={list.card}>
-          <a href="">
-            <img src={picture} alt="" />
-          </a>
-          <div>Leslie</div>
-        </div>
-
-        <div className={list.card}>
-          <a href="">
-            <img src={picture} alt="" />
-          </a>
-          <div>Leslie</div>
-        </div>
-
-        <div className={list.card}>
-          <a href="">
-            <img src={picture} alt="" />
-          </a>
-          <div>Leslie</div>
-        </div>
-
-      </div>
-    )
   }
 }
--- 10-ReactScaffold/search-in-github/src/Components/Search/Search.jsx
+++ 10-ReactScaffold/search-in-github/src/Components/Search/Search.jsx
@@ -18,15 +18,20 @@ export default class Search extends Component {
   search = () => {
     // 获取用户的输入
     const { value: keyword } = this.keywordElem
-
+    // 发送请求前,通知App组件更新状态
+    this.props.updateAppState({ isFirst: false, isLoading: true })
     // 发送网络请求
     axios.get(`https://api.github.com/search/users?q=${keyword}`)
       .then(
         response => {
+          // 请求成功后，通知App组件更新状态
           console.log(response.data);
+          this.props.updateAppState({isLoading: false, users: response.data.items})
         },
         error => {
+          // 请求失败后，通知App组件更新状态
           console.log(error);
+          this.props.updateAppState({isLoading: false, err: error.message})
         }
       )
   }
