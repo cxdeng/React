8320913b5187a12066746380317cc94288df51ec
cxdeng <chengx.deng@icloud.com>
2023-08-25 10:09:00 +0100

    Nested Routing

---
+++ 11-ReactRouter/nested-routing/src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import { Route, Redirect, Switch } from 'react-router-dom'
+import Title from './components/Title/Title'
+import Nav from './components/Nav/Nav'
+import Home from './pages/Home/Home'
+import About from './pages/About/About'
+import './App.css'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='main'>
+        <Title></Title>
+
+        <div className='container'>
+          <Nav></Nav>
+
+          {/* 注册路由 */}
+          <Switch>
+            <Route path='/about' component={About}></Route>
+            <Route path='/home' component={Home}></Route>
+            <Redirect to='/about' />
+          </Switch>
+        </div>
+      </div>
+    )
+  }
+}
